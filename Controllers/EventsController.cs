26621c3db80d7195526711308547b7343b1d4bac
jpascua5 <JPascua5@ITS.JNJ.com>
2019-11-13 13:40:04 -0500

    XML Reader, different approach in the controller

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace ERDSPortal.Controllers
 {
@@ -19,18 +20,20 @@ namespace ERDSPortal.Controllers
             List<Models.Events> events = new List<Models.Events>();
             XmlDocument doc = new XmlDocument();
             doc.Load(Server.MapPath("~/Content/events.xml"));
-            foreach (XmlNode node in doc.SelectNodes("/EVENTLIST/EVENT"))
+
+            foreach (XmlElement element in doc.DocumentElement.SelectNodes("EVENT[ID='1']"))
             {
-                events.Add(new Models.Events
+                Console.Out.WriteLine(doc.OuterXml);
+                /*events.Add(new Models.Events
                 {
                     EventID = int.Parse(node["ID"].InnerText),
                     Title = node["TITLE"].InnerText,
                     Date = node["DATE"].InnerText,
                     Location = node["LOCATION"].InnerText,
                     Details = node["DETAILS"].InnerText
-                });
+                });*/
             }
-            return View(events);
+            return View();
         }
     }
 }
