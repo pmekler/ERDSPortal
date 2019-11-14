178512cdc7e7823662f9a51cb6caa72455f71a3f
Paul Mekler <PMekler@its.jnj.com>
2019-11-14 13:03:14 -0500

    XML id look up for events

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml;
-using System.Xml.Linq;
 
 namespace ERDSPortal.Controllers
 {
@@ -20,20 +19,18 @@ namespace ERDSPortal.Controllers
             List<Models.Events> events = new List<Models.Events>();
             XmlDocument doc = new XmlDocument();
             doc.Load(Server.MapPath("~/Content/events.xml"));
-
-            foreach (XmlElement element in doc.DocumentElement.SelectNodes("EVENT[ID='1']"))
+            foreach (XmlNode node in doc.SelectNodes("/Events/Event"))
             {
-                Console.Out.WriteLine(doc.OuterXml);
-                /*events.Add(new Models.Events
+                events.Add(new Models.Events
                 {
                     EventID = int.Parse(node["ID"].InnerText),
                     Title = node["TITLE"].InnerText,
                     Date = node["DATE"].InnerText,
                     Location = node["LOCATION"].InnerText,
                     Details = node["DETAILS"].InnerText
-                });*/
+                });
             }
-            return View();
+            return View(events);
         }
     }
 }
